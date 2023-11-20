bf1ade8e214eebe155bd15d2ca9514c4842d5bff
iayanpahwa <iayanpahwa@gmail.com>
2023-11-20 23:59:52 +0530

    disable web debugging in main app

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def links():
     return render_template("index.html", url=url[:-1])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
 
     
